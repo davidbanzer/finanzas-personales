971f577243a0d0eaa63a8a0470ce2748d6233a5c
David Banzer <davidbanzer123@gmail.com>
2023-10-19 13:01:42 -0400

    abm cuentas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,7 +34,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'movements',
+    loadChildren: () => import('./pages/movements/movements.module').then( m => m.MovementsPageModule)
   },
+
 
 
 ];
